ed0d70829cb2b2f57f488a433f17005faebbff28
keyejia <keyejia@gmail.com>
2018-07-05 03:58:10 +0000

    added App and base home page

--- product_hunt_clone/settings.py
+++ product_hunt_clone/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'products.apps.ProductsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'product_hunt_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['product_hunt_clone/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
