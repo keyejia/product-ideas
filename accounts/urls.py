520d4be0522ee5a5f8d306de9812a102329dc6c1
keyejia <keyejia@gmail.com>
2018-07-08 00:36:33 +0000

    Chore: completed login and logout pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('logout', views.logout, name = 'logout'),
-    path('login', views.login, name = 'lgoin'),
+    path('login', views.login, name = 'login'),
     path('signup', views.signup, name = 'signup'),
 ]
